a3aee30fe43c198cff7cf950525bc38aee43018c
Justin Mayfield <tooker@gmail.com>
2016-06-09 15:14:57 -0600

    Cleanup

--- gelf_elasticsearch.py
+++ gelf_elasticsearch.py
@@ -4,33 +4,53 @@ import aiohttp
 import asyncio
 import gzip
 import json
-import sys
+import shellish
 
 loop = asyncio.get_event_loop()
-es = aiohttp.ClientSession(loop=loop)
 
 
 class GelfServerProtocol(object):
 
+    def connection_made(self, transport):
+        pass
+
     def datagram_received(self, data, addr):
         log = json.loads(gzip.decompress(data).decode())
+        if self.verbose:
+            shellish.vtmlprint('<b>LOG:<b>', log)
         asyncio.ensure_future(self.relaylog(log))
 
     async def relaylog(self, log):
+        log['timestamp'] = round(log['timestamp'] * 1000)
+        data = json.dumps(log)
         with aiohttp.Timeout(60):
-            log['timestamp'] = round(log['timestamp'] * 1000)
-            async with es.post(sys.argv[1], data=json.dumps(log)) as response:
-                print(await response.text())
+            async with self.es_session.post(self.es_url, data=data) as r:
+                if r.status != 201:
+                    raise Exception(await r.text())
+                if self.verbose:
+                    shellish.vtmlprint('<b>ES INDEX:</b>', await r.text())
 
 
-listen = loop.create_datagram_endpoint(GelfServerProtocol,
-                                       local_addr=('0.0.0.0', 12201))
-transport, protocol = loop.run_until_complete(listen)
+@shellish.autocommand
+def gelf_es_relay(elasticsearch_url, listen_addr='0.0.0.0', listen_port=12201,
+                  verbose=False):
+    """ A Gelf server relay to elasticsearch.
 
-try:
-    loop.run_forever()
-except KeyboardInterrupt:
-    pass
+    The URL should contain the /index/type args as per the elasticsearch API.
 
-transport.close()
-loop.close()
+    E.g. https://elasticsearch/logs/docker
+    """
+    addr = listen_addr, listen_port
+    listen = loop.create_datagram_endpoint(GelfServerProtocol, local_addr=addr)
+    transport, protocol = loop.run_until_complete(listen)
+    protocol.verbose = verbose
+    protocol.es_session = aiohttp.ClientSession(loop=loop)
+    protocol.es_url = elasticsearch_url
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        pass
+    transport.close()
+    loop.close()
+
+gelf_es_relay()
